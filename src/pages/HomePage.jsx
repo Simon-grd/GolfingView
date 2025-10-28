c673678fca27d189d3c9abc5c11e5693be25cbdf
Scimontist <simon.godard@ecoles-epsi.net>
2025-10-28 09:52:35 +0100

    mise en page et style

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="mb-4">GolfingView ⛳</h1>
+      <p>Bienvenue sur l’application des passionnés de golf !</p>
+      <p>Recherchez les parcours, suivez les compétitions et lisez les dernières actualités.</p>
+    </div>
+  );
+}
